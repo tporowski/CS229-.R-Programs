ffd4d5afa67f2d62aacbaf11c0e29fe040814254
Thomas Porowski <tporowski@arcadia.edu>
2024-09-05 13:32:32 -0400

    Add files via upload

---
+++ Classwork (Dynamic).R
@@ -0,0 +1,42 @@
+# #C2
+# vector_up = seq(1, 20)
+# vector_down = seq(19, 1)
+# vector = c(vector_up, vector_down)
+# print(vector)
+# 
+# #C3
+# temp = c(4, 6, 3)
+# print(temp)
+# 
+# #C4
+# vector = c()
+# vector = rep(temp, times=10)
+# print(vector)
+# 
+# #C5
+# vector = c()
+# vector = rep(temp, times= seq(10, 30, by=10))
+# print(vector)
+# 
+# #C6
+# boolean_vector = c(T, F, T)
+# print(boolean_vector)
+# 
+# #E1
+# power_vector = seq(3, 6, by = 0.1)
+# e_vector = exp(power_vector)
+# print(e_vector)
+# 
+# #E2
+# vector = seq(1, 10)
+# change = ((-1)^vector)*(vector^3)
+# print(change)
+# 
+# #F1
+# my_stock = c(213.28, 209.19, 205.70, 208.74, 209.01)
+# week_days = c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+# negative_stocks = my_stock < 209
+# week_days[negative_stocks]
+
+#G1
+matrix(1:9, 3)
