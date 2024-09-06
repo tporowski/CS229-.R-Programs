0d9e825f4ba8cd07c75a8bd470537da22ef5afa3
Thomas Porowski <tporowski@arcadia.edu>
2024-09-05 23:03:17 -0400

    Add files via upload

---
+++ CA1_ThomasPorowski.R
@@ -0,0 +1,34 @@
+# Question 1 --------------------------------------------------------------
+## (1)
+# please write your answer below by replacing the current values
+square_root <- 0 # TODO replace 0 with your answer
+print(square_root)
+
+## (2)
+# please write your answer below by replacing the current values
+fraction <- 0 # TODO replace 0 with your answer
+print(fraction)
+
+
+# Question 2 --------------------------------------------------------------
+## (1)
+# TODO please write your answer below
+
+## (2)
+# TODO please write your answer below
+
+## (3)
+# please write your answer below by replacing the current value
+missing_word <- "blank" # TODO replace blank with the missing word
+print(missing_word)
+
+
+# Question 3 --------------------------------------------------------------
+## (1)
+# TODO please write your answer below
+
+## (2)
+# TODO please write your answer below
+
+## (3)
+# TODO please write your answer below
