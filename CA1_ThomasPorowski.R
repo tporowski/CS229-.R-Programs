23b29172512d58ba9ac706162ac0896a61c3d654
Thomas Porowski <tporowski@arcadia.edu>
2024-09-06 12:29:35 -0400

    Update CA1_ThomasPorowski.R

--- CA1_ThomasPorowski.R
+++ CA1_ThomasPorowski.R
@@ -1,34 +1,42 @@
+rm(list=ls())
 # Question 1 --------------------------------------------------------------
 ## (1)
 # please write your answer below by replacing the current values
-square_root <- 0 # TODO replace 0 with your answer
+square_root <- sqrt(log(6)) # TODO replace 0 with your answer
 print(square_root)
 
 ## (2)
 # please write your answer below by replacing the current values
-fraction <- 0 # TODO replace 0 with your answer
+fraction <- ((15.3)^2)/abs(9.7-10.2) # TODO replace 0 with your answer
 print(fraction)
 
 
 # Question 2 --------------------------------------------------------------
 ## (1)
 # TODO please write your answer below
+#install.packages('tidyverse')
+
 
 ## (2)
 # TODO please write your answer below
+library('tidyverse')
 
 ## (3)
 # please write your answer below by replacing the current value
-missing_word <- "blank" # TODO replace blank with the missing word
+#?drop_na()
+missing_word <- "rows" # TODO replace blank with the missing word
 print(missing_word)
 
 
 # Question 3 --------------------------------------------------------------
 ## (1)
 # TODO please write your answer below
+print(my_msg <- 'Hello World')
 
 ## (2)
 # TODO please write your answer below
+print(this_year <- 2024)
 
 ## (3)
 # TODO please write your answer below
+print(division <- this_year / 4)
